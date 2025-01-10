0fc174157aaab9ffbebd36549fb963ed17f53dba
Cryptix Coin Network <info@king-krypto.de>
2025-01-10 22:53:12 +0100

    fix

--- crypto/hashes/src/pow_hashers.rs
+++ crypto/hashes/src/pow_hashers.rs
@@ -10,6 +10,7 @@ impl PowHash {
     // The initial state of `cSHAKE256("ProofOfWorkHash")`
     // [10] -> 1123092876221303310 ^ 0x04(padding byte) = 1123092876221303306
     // [16] -> 10306167911662716186 ^ 0x8000000000000000(final padding) = 1082795874807940378
+    
     #[rustfmt::skip]
     const INITIAL_STATE: [u64; 25] = [
         1242148031264380989, 3008272977830772284, 2188519011337848018, 1992179434288343456, 8876506674959887717,
@@ -18,6 +19,7 @@ impl PowHash {
         3784524041015224902, 1082795874807940378, 13952716920571277634, 13411128033953605860, 15060696040649351053,
         9928834659948351306, 5237849264682708699, 12825353012139217522, 6706187291358897596, 196324915476054915,
     ];
+
     #[inline]
     pub fn new(pre_pow_hash: Hash, timestamp: u64) -> Self {
         let mut start = Self::INITIAL_STATE;
@@ -31,10 +33,23 @@ impl PowHash {
     #[inline(always)]
     pub fn finalize_with_nonce(mut self, nonce: u64) -> Hash {
         self.0[9] ^= nonce;
+    
+
         keccak256::f1600(&mut self.0);
-        Hash::from_le_u64(self.0[..4].try_into().unwrap())
+    
+        // Hello ASICs - try to eat this.
+        // Info: Cryptix will never accept or support ASICs. This is just the first step. In the future, multiple iterations may be added, or even other algorithms,
+        // up to the potential integration of RandomX. So don't waste your time and money building an ASIC miner, it won't even work for a week, and the algorithm will 
+        //be changed as soon as there is even a suspicion of ASIC involvement.
+
+        let mut second_hash = self.0.clone();  
+        keccak256::f1600(&mut second_hash);  
+    
+        Hash::from_le_u64(second_hash[..4].try_into().unwrap())
     }
+    
 }
+
 
 impl KHeavyHash {
     // The initial state of `cSHAKE256("HeavyHash")`
