8a1cec871f3af8552604312b5ac0ba70831c0ee5
Cryptix Network <info@king-krypto.de>
2025-01-18 19:21:02 +0100

    Update lib.rs

--- consensus/pow/src/lib.rs
+++ consensus/pow/src/lib.rs
@@ -50,17 +50,18 @@ impl State {
     /// PRE_POW_HASH || TIME || 32 zero byte padding || NONCE
     pub fn calculate_pow(&self, nonce: u64) -> Uint256 {
         let hash = self.hasher.clone().finalize_with_nonce(nonce);
-        let hash_bytes: [u8; 32] = *hash.as_bytes();
-
+        let hash_bytes: [u8; 32] = hash.as_bytes().try_into().expect("Hash output length mismatch");
+    
         let mut sha3_hasher = Sha3_256::new();
         sha3_hasher.update(hash_bytes);
         let sha3_hash = sha3_hasher.finalize();
         let sha3_hash_bytes: [u8; 32] = sha3_hash.as_slice().try_into().expect("SHA-3 output length mismatch");
-
+    
         let final_hash = self.matrix.heavy_hash(cryptix_hashes::Hash::from(sha3_hash_bytes));
-
+    
         Uint256::from_le_bytes(final_hash.as_bytes())
     }
+    
 
     #[inline]
     #[must_use]
